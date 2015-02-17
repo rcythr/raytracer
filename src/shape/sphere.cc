e15753c480896920068443a6d5c6964d350931a7
Mike Fink <mfink351@mfink351.wireless.rit.edu>
2015-02-17 12:51:28 -0500

    Code working for sphere hit detection

--- src/shape/sphere.cc
+++ src/shape/sphere.cc
@@ -5,12 +5,38 @@
 
 #include <iostream>
 #include <sstream>
+#include <math.h> 
 
 using namespace raytracer;
 
 bool Sphere::test_hit(Ray& ray, double& tmin)
 {
-    return false;
+
+	//Major values for texting interstion
+	//a, b, and c are part of the intersection equation
+	glm::vec3 oc;
+
+	oc = ray.origin-point;
+
+	//b=2(l*(o-c))
+	auto b = glm::dot(ray.direction,oc)*2.0f;
+	//c = (o-c)^2-r^2
+	auto c = dot(oc,oc)- radius*radius;
+	//testVal = b^2-c;
+	auto testVal = b*b - c*4.0f;
+	//float x, y, z;
+	//x = testVec.x; y = testVec.y; z=testVec.z;
+	//float testVal = sqrt(x*x+y*y+z*z);
+
+	//if b^2-c >= 0
+	if(testVal == 0.0){
+		return true;
+	} else if(testVal > 0.0) {
+		return true;
+	}
+	else{
+    	return false;
+    }
 }
 
 std::string Sphere::toString(size_t depth)
