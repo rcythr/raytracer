62b46c53650b14f39b72aab21ff1561559265e39
Mike Fink <m.fink351@gmail.com>
2015-04-29 17:34:00 -0400

    Created random generation of rays from a triangle

--- src/light/triangular.hpp
+++ src/light/triangular.hpp
@@ -14,6 +14,42 @@ namespace raytracer {
 
         virtual LightType get_type() override { return LightType::TRIANGULAR; }
 
+		virtual std::vector<Ray> get_photons(int n){
+				//Create Ray vector
+				vector<Ray> rays;
+
+				//Create random device for uniform distribution
+				std::random_device rd;
+				std::mt19937 gen(rd());
+				std::uniform_real_distribution<> dis(0, 1);
+
+				for(int i = 0; i < n; i++){
+					//Create ray
+					Ray r;
+					//Generate origin
+					float r1 = dis(gen), r2 = dis(gen);
+					r->origin = (1-sqrt(r1)) * triangle->p0 +
+								(sqrt(r1)*(1-r2)) * triangle->p1 +
+								(sqrt(r1) * r2) * triangle->p2;
+					//Generate direction
+					r1 = dis(gen), r2 = dis(gen);
+					glm::vec3 randPoint = (1-sqrt(r1)) * triangle->p0 +
+								(sqrt(r1)*(1-r2)) * triangle->p1 +
+								(sqrt(r1) * r2) * triangle->p2;
+					//Generate normal
+					glm::vec3 normal = glm::normalize(glm::cross((triangle->p1-triangle->p0),
+																(triangle->p2-triangle-p0));
+					glm::vec3 direction = randPoint + normal;
+
+					//Set the direction
+					r.direction = direction - r.origin;
+
+					//Update and push ray
+					r.update();
+					rays.push_back(r);
+				}
+		};
+
         std::shared_ptr<Triangle> triangle;
     };
 }
