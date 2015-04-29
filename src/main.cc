d757c134549112469fc86afc35bdcfeccb6c932f
Richard Laughlin <rwl3564@rit.edu>
2015-04-29 17:54:30 -0400

    Testing photon generation.

--- src/main.cc
+++ src/main.cc
@@ -9,6 +9,21 @@
 using namespace raytracer;
 
 int main(int argc, char* argv[]) {
+
+    auto triangle = std::make_shared<Triangle>(
+        glm::vec3(0.0f, 0.0f, 0.0f), glm::vec2(0.0f),
+        glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(0.0f),
+        nullptr);
+
+    auto light = std::make_shared<TriangularLight>(1000.f, triangle, glm::vec3(0.0f));
+
+    auto rays = light->get_photons(100000);
+    for(auto& ray : rays)
+    {
+        std::cout << "Origin: " << ray.origin << " | Direction: " << ray.direction << std::endl;
+    }
+
     if (argc != 2) {
         std::cout << "Arguments: raytracer [config_filename]" << std::endl;
         return EXIT_FAILURE;
