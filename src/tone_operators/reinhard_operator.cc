e3bf6c1e420bc744cdee53bd05883b784774a6c6
Richard Laughlin <rwl3564@rit.edu>
2015-05-15 02:12:39 -0400

    Finished tone reproduction implementation.

--- src/tone_operators/reinhard_operator.cc
+++ src/tone_operators/reinhard_operator.cc
@@ -1,6 +1,30 @@
 #include "tone_operators/reinhard_operator.hpp"
+#include "util/log_avg_luminance.hpp"
 
 using namespace raytracer;
 
 void ReinhardOperator::apply(ViewPlanePtr vp) {
+    auto width = vp->get_width();
+    auto height = vp->get_height();
+
+    auto L_wa = avg_luminance(vp, l_max);
+    
+    float a_lw = 0.18f / L_wa;
+    for(int64_t i=0; i < height; ++i)
+    {
+        for(int64_t j=0; j < width; ++j)
+        {
+            auto color = vp->get_pixel(i, j) * l_max;
+
+            color.r = a_lw * color.r;
+            color.g = a_lw * color.g;
+            color.b = a_lw * color.b;
+            
+            color.r = color.r / (1.0f + color.r);
+            color.g = color.g / (1.0f + color.g);
+            color.b = color.b / (1.0f + color.b);
+
+            vp->set_pixel(i, j, color);
+        }
+    }
 }
--- src/tone_operators/ward_operator.cc
+++ src/tone_operators/ward_operator.cc
@@ -1,6 +1,20 @@
 #include "tone_operators/ward_operator.hpp"
+#include "util/log_avg_luminance.hpp"
 
 using namespace raytracer;
 
 void WardOperator::apply(ViewPlanePtr vp) {
+    auto width = vp->get_width();
+    auto height = vp->get_height();
+
+    auto L_wa = log_avg_luminance(vp, l_max);
+    auto sf = std::pow((1.219f + std::pow(100.f / 5.f, 0.4f)) / (1.219f + std::pow(L_wa, 0.4f)), 2.5f);
+
+    for(int64_t i=0; i < height; ++i)
+    {
+        for(int64_t j=0; j < width; ++j)
+        {
+            vp->set_pixel(i, j, sf * vp->get_pixel(i, j));
+        }
+    }
 }
